0d1e04b70e764887a22c603bde5db3e7ef08e75e
Éverton Arruda <root@earruda.eti.br>
2012-08-24 10:52:11 -0400

    Adding absolute path to last-tweeted-entry filename
    When adding TweetMyPlanet to cron, the last-tweeted-entry
    file was being created in the user's home directory.
    
    Signed-off-by: Éverton Arruda <root@earruda.eti.br>

--- configuration.py
+++ configuration.py
@@ -1,3 +1,5 @@
+import os
+
 # Configurations File
 class Configuration:
     # Url of the Planet's RSS Feed
@@ -15,5 +17,9 @@ class Configuration:
     twitterAccessToken = ""
     twitterAccessTokenSecret = ""
 
-    # Last-Tweeted-Entry filename. File that will store the last tweeted entry
-    lastTweetedEntryFilename = "last-tweeted-entry"
+    # Get the path of the tweetMyPlanet script
+    __confFilename = os.path.realpath(__file__).split("/")[-1]
+    __tweetMyPlanetDir = os.path.realpath(__file__).strip(__confFilename)
+
+    # Path to Last-Tweeted-Entry file. File that stores the last tweeted entry
+    lastTweetedEntryFilename = __tweetMyPlanetDir + "last-tweeted-entry"
